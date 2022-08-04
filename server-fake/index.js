a618acbaadc840741e91310485edadc3c86650be
Thiago Fassina <thiagoaaa@gmail.com>
2022-08-04 17:26:50 -0300

    Add empty endpoints

--- server-fake/index.js
+++ server-fake/index.js
@@ -3,8 +3,48 @@ import express from 'express';
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('ok')
+app.post('/api/v1/account/create', (_, res) => {
+  res.send('/api/v1/account/create')
+})
+
+app.post('/api/v1/account/login', (_, res) => {
+  res.send('/api/v1/account/login')
+})
+
+app.get('/api/v1/app', (_, res) => {
+  res.send('/api/v1/app')
+})
+
+app.post('/api/v1/app', (_, res) => {
+  res.send('/api/v1/app')
+})
+
+app.get('/api/v1/bucket', (_, res) => {
+  res.send('/api/v1/bucket')
+})
+
+app.post('/api/v1/bucket', (_, res) => {
+  res.send('/api/v1/bucket')
+})
+
+app.put('/api/v1/bucket', (_, res) => {
+  res.send('/api/v1/bucket')
+})
+
+app.get('/api/v1/schema', (_, res) => {
+  res.send('/api/v1/schema')
+})
+
+app.post('/api/v1/schema', (_, res) => {
+  res.send('/api/v1/schema')
+})
+
+app.get('/api/v1/object', (_, res) => {
+  res.send('/api/v1/object')
+})
+
+app.post('/api/v1/object', (_, res) => {
+  res.send('/api/v1/object')
 })
 
 app.listen(port, () => {
