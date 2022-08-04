e187668f67b7e06b36cbdc36d08bf1e82d23b684
Thiago Fassina <thiagoaaa@gmail.com>
2022-08-04 17:44:07 -0300

    Add initial logic to server-fake

--- server-fake/index.js
+++ server-fake/index.js
@@ -1,52 +1,78 @@
 import express from 'express';
-
+import bodyParser from 'body-parser';
+import _ from 'lodash';
 const app = express()
+app.use(bodyParser.json())
+
+const db = {
+  accounts: [],
+  apps: [],
+
+};
+
+app.post('/api/v1/account/create', (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    res.json({success: false})
+    return
+  }
+
+  db.accounts.push({
+    username: req.body.username,
+    password: req.body.password,
+  })
+
+  res.json({success: true})
+})
+
+app.post('/api/v1/account/login', (req, res) => {
+  const account = _.find(db.accounts, {username: req.body.username})
+
+  if (!account || account.password !== req.body.password) {
+    res.json({success: false})
+    return
+  }
+
+  res.json({success: true})
+})
+
+app.get('/api/v1/app', (req, res) => {
+  res.json([])
+})
+
+app.post('/api/v1/app', (req, res) => {
+  res.json({success: false})
+})
+
+app.get('/api/v1/bucket', (req, res) => {
+  res.json([])
+})
+
+app.post('/api/v1/bucket', (req, res) => {
+  res.json({success: false})
+})
+
+app.put('/api/v1/bucket', (req, res) => {
+  res.json({success: false})
+})
+
+app.get('/api/v1/schema', (req, res) => {
+  res.json([])
+})
+
+app.post('/api/v1/schema', (req, res) => {
+  res.json({success: false})
+})
+
+app.get('/api/v1/object', (req, res) => {
+  res.json([])
+})
+
+app.post('/api/v1/object', (req, res) => {
+  res.json({success: false})
+})
+
+
 const port = 8080
-
-app.post('/api/v1/account/create', (_, res) => {
-  res.send('/api/v1/account/create')
-})
-
-app.post('/api/v1/account/login', (_, res) => {
-  res.send('/api/v1/account/login')
-})
-
-app.get('/api/v1/app', (_, res) => {
-  res.send('/api/v1/app')
-})
-
-app.post('/api/v1/app', (_, res) => {
-  res.send('/api/v1/app')
-})
-
-app.get('/api/v1/bucket', (_, res) => {
-  res.send('/api/v1/bucket')
-})
-
-app.post('/api/v1/bucket', (_, res) => {
-  res.send('/api/v1/bucket')
-})
-
-app.put('/api/v1/bucket', (_, res) => {
-  res.send('/api/v1/bucket')
-})
-
-app.get('/api/v1/schema', (_, res) => {
-  res.send('/api/v1/schema')
-})
-
-app.post('/api/v1/schema', (_, res) => {
-  res.send('/api/v1/schema')
-})
-
-app.get('/api/v1/object', (_, res) => {
-  res.send('/api/v1/object')
-})
-
-app.post('/api/v1/object', (_, res) => {
-  res.send('/api/v1/object')
-})
-
 app.listen(port, () => {
   console.log(`server-fake listening on port ${port}`)
 })
