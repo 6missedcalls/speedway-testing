abf5ef2404b9108cf8ea8accbff6951549c5c91a
Thiago Fassina <thiagoaaa@gmail.com>
2022-08-10 19:03:50 -0300

    make call to endpoint on login submit

--- src/pages/Login/Container.tsx
+++ src/pages/Login/Container.tsx
@@ -2,7 +2,13 @@ import LoginComponent from "./Component"
 
 function LoginContainer() {
 	const login = (walletAddress: string, password: string) => {
-		console.log(walletAddress, password)
+		fetch("http://localhost:8080/api/v1/account/login", {
+			method: "POST",
+			headers: { "content-type": "application/json" },
+			body: JSON.stringify({ did: walletAddress, password }),
+		})
+			.then((response) => response.json())
+			.then((body) => alert(JSON.stringify(body)))
 	}
 
 	return <LoginComponent onSubmit={login} />
