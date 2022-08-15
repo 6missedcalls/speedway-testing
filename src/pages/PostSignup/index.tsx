0306478d67918947c24be51a5d038328b323c009
Thiago Fassina <thiagoaaa@gmail.com>
2022-08-15 14:45:01 -0300

    navigate after complete account creation

--- src/pages/PostSignup/index.tsx
+++ src/pages/PostSignup/index.tsx
@@ -1,7 +1,8 @@
 import copy from "copy-to-clipboard"
-import { useLocation } from "react-router"
+import { useLocation, useNavigate } from "react-router-dom"
 
 const Component = () => {
+	const navigate = useNavigate()
 	const { state } = useLocation()
 	const { Did } = state as { Did: string }
 	return (
@@ -10,21 +11,28 @@ const Component = () => {
 				<h1 className="uppercase text-4xl font-extrabold mb-10 break-words leading-tight">
 					{Did}
 				</h1>
+
 				<p className="text-xl mb-10">
 					Welcome! This is your SonrID. Copy it or write it down, and store it
 					somewhere where you will not lose it.
 				</p>
+
 				<p className="text-xl mb-10">
 					If you lose or forget your SonrID you will lose access to your account
 					forever.
 				</p>
+
 				<button
 					onClick={() => copy(Did)}
 					className="block py-3 rounded border border-primary w-full text-xl text-button-outlined font-semibold mb-4 active:opacity-60"
 				>
 					Copy SonrID to Clipboard
 				</button>
-				<button className="block py-3 rounded bg-primary w-full text-xl font-semibold">
+
+				<button
+					onClick={() => navigate("/objects")}
+					className="block py-3 rounded bg-primary w-full text-xl font-semibold"
+				>
 					Complete Registration
 				</button>
 			</div>
