345e1e4239ea03a74daece4ee634aac45fba7333
Thiago Fassina <thiagoaaa@gmail.com>
2022-08-18 18:58:25 -0300

    add ability to test endpoints

--- server-in-memory/test.js
+++ server-in-memory/test.js
@@ -1,3 +1,16 @@
-it("testing suite", () => {
-	expect(1).toBe(1)
+import storage from "node-persist"
+import app from "./server.js"
+import supertest from "supertest"
+const request = supertest(app)
+
+beforeAll(async () => {
+	await storage.init({
+		dir: ".node-persist-test",
+	})
+})
+
+it("test endpoint", async () => {
+	const { body } = await request.post("/api/v1/account/create")
+	expect(Object.keys(body)).toEqual(["Address"])
+	expect(typeof body.Address).toBe("string")
 })
