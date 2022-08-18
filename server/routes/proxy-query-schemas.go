d880f740d11564fdbc3689f411e35587dd6fcc1b
brokecollegedevwithaclothingobsession <supremehitsheads@gmail.com>
2022-08-18 16:43:10 -0700

    chore: addition of proxy-query-schemas

---
+++ server/routes/proxy-query-schemas.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (ns *NebulaServer) ProxyQuerySchemas(c *gin.Context) {
+	c.Redirect(http.StatusMovedPermanently, "http://v1-beta.sonr.ws:1317/sonr-io/sonr/schema/query/all_schemas?"+c.Request.URL.RawQuery)
+}
