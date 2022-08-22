56eb70daab508c93fa0228d459c52fed519085dd
Thiago Fassina <thiagoaaa@gmail.com>
2022-08-22 16:54:09 -0300

    add title and button to buckets page

--- src/pages/Buckets/Component.tsx
+++ src/pages/Buckets/Component.tsx
@@ -3,7 +3,19 @@ import LayoutMenu from "../../components/LayoutMenu"
 function BucketsPageComponent() {
 	return (
 		<LayoutMenu>
-			<div>Buckets page</div>
+			<div className="h-screen bg-gray-100">
+				<div className="py-14 px-10">
+					<div className="flex flex-row">
+						<h1 className="flex-1 text-custom-3xl font-extrabold tracking-custom-x2tighter">
+							Buckets
+						</h1>
+
+						<button className="self-center text-skin-primary font-extrabold bg-skin-primary rounded py-2 px-6">
+							Create Bucket
+						</button>
+					</div>
+				</div>
+			</div>
 		</LayoutMenu>
 	)
 }
