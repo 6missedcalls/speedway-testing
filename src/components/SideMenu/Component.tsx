3f78e18ed46d624ccbc6366d4f5b4e2c87ea58c5
Thiago Fassina <thiagoaaa@gmail.com>
2022-08-22 16:38:05 -0300

    add bucket button to side menu

--- src/components/SideMenu/Component.tsx
+++ src/components/SideMenu/Component.tsx
@@ -4,7 +4,7 @@ import { NavigateFunction } from "react-router-dom"
 import {
 	ROUTE_SCHEMAS,
 	// ROUTE_OBJECTS,
-	// ROUTE_BUCKETS,
+	ROUTE_BUCKETS,
 } from "../../utils/constants"
 import { useSelector } from "react-redux"
 import { selectAddress } from "../../redux/slices/authenticationSlice"
@@ -27,11 +27,11 @@ const modulesButtons = [
 	// 	iconName: "Document",
 	// 	route: ROUTE_OBJECTS,
 	// },
-	// {
-	// 	label: "Buckets",
-	// 	iconName: "Document",
-	// 	route: ROUTE_BUCKETS,
-	// },
+	{
+		label: "Buckets",
+		iconName: "Bag2",
+		route: ROUTE_BUCKETS,
+	},
 ]
 
 function SideMenuComponent({ navigate, currentPath }: SideMenuComponentProps) {
