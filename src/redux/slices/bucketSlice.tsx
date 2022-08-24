f35b15dae7e7ba6695be486c7c4127680b1055f2
Thiago Fassina <thiagoaaa@gmail.com>
2022-08-24 18:06:07 -0300

    create bucket modal loading state

--- src/redux/slices/bucketSlice.tsx
+++ src/redux/slices/bucketSlice.tsx
@@ -7,6 +7,10 @@ export const selectBuckets = (state: RootState) => {
 	return state.bucket.list
 }
 
+export const selectBucketCreationLoading = (state: RootState) => {
+	return state.bucket.creating
+}
+
 export const getAllBuckets = createAsyncThunk("buckets/getAll", async () => {
 	return await fetch(`${BASE_API}/bucket/all`, {
 		method: "POST",
@@ -27,9 +31,11 @@ export const createBucket = createAsyncThunk(
 
 export type BucketState = {
 	list: Bucket[]
+	creating: boolean
 }
 export const initialState: BucketState = {
 	list: [],
+	creating: false,
 }
 const bucketSlice = createSlice({
 	name: "bucket",
@@ -40,7 +46,14 @@ const bucketSlice = createSlice({
 			state.list = action.payload.data
 		})
 
+		builder.addCase(createBucket.pending, (state, action) => {
+			state.creating = true
+		})
+		builder.addCase(createBucket.rejected, (state, action) => {
+			state.creating = false
+		})
 		builder.addCase(createBucket.fulfilled, (state, action) => {
+			state.creating = false
 			state.list.push(action.payload)
 		})
 	},
