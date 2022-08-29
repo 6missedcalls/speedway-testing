92c411eb3fb9de3e799cb26c3d72e38479b279ce
Thiago Fassina <thiagoaaa@gmail.com>
2022-08-29 10:03:03 -0300

    update object list on create

--- src/pages/Objects/components/NewObjectModalContent/Container.tsx
+++ src/pages/Objects/components/NewObjectModalContent/Container.tsx
@@ -6,7 +6,10 @@ import {
 	selectBuckets,
 	updateBucket,
 } from "../../../../redux/slices/bucketSlice"
-import { userCreateObject } from "../../../../redux/slices/objectsSlice"
+import {
+	userCreateObject,
+	userGetBucketObjects,
+} from "../../../../redux/slices/objectsSlice"
 import { userGetSchema } from "../../../../redux/slices/schemasSlice"
 import { AppDispatch } from "../../../../redux/store"
 import { IobjectPropertyChange, Ischema } from "../../../../utils/types"
@@ -97,6 +100,8 @@ function NewObjectModalContentContainer({
 		}
 
 		await dispatch(updateBucket({ ...bucketUpdatePayload }))
+
+		dispatch(userGetBucketObjects({ bucket: selectedBucket }))
 
 		closeModal()
 	}
