3e33c91b0ae1f8e634c031a123e501c28635af4c
josh long <josh@sonr.io>
2022-08-30 12:58:54 -0400

    addition of bucket item prompts on create

--- internal/prompts/prompts.go
+++ internal/prompts/prompts.go
@@ -2,9 +2,12 @@ package prompts
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/kataras/golog"
 	"github.com/manifoldco/promptui"
 	rtmv1 "github.com/sonr-io/sonr/third_party/types/motor"
+	btv1 "github.com/sonr-io/sonr/x/bucket/types"
 	"github.com/sonr-io/speedway/internal/status"
 	"github.com/sonr-io/speedway/internal/storage"
 )
@@ -71,6 +74,54 @@ func QuitSelector(label string) bool {
 		return false
 	}
 	return true
+}
+
+func LabelPrompt(label string, logger *golog.Logger) string {
+	prompt := promptui.Prompt{
+		Label: label,
+	}
+	label, err := prompt.Run()
+
+	if err != nil {
+		logger.Fatalf("Error while running label prompt: %s", err)
+	}
+
+	return label
+}
+
+func ResourceIdentifierPrompt(logger *golog.Logger) string {
+	prompt := promptui.Select{
+		Label: "Select the resource identifer",
+		Items: []string{
+			"None",
+			"DID",
+			"CID",
+		},
+	}
+
+	_, value, _ := prompt.Run()
+
+	return value
+}
+
+func BucketContentPrompt(logger *golog.Logger) []*btv1.BucketItem {
+	var content []*btv1.BucketItem
+	var rerun = false
+	for rerun != true {
+		name := LabelPrompt("enter content name", logger)
+		uri := LabelPrompt("URI for content", logger)
+		ts := time.Now().Unix()
+		_ = ResourceIdentifierPrompt(logger)
+		content = append(content, &btv1.BucketItem{
+			Name:      name,
+			Uri:       uri,
+			Timestamp: ts,
+			Type:      btv1.ResourceIdentifier_CID,
+		})
+		rerun = QuitSelector("Would you like to add another item?")
+	}
+
+	return content
 }
 
 func SplashScreen() string {
