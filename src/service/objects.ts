5104c5a2af46a5af1f3c240114077e498d20b358
Thiago Fassina <thiagoaaa@gmail.com>
2022-09-05 14:49:40 -0300

    delete unused service method

--- src/service/objects.ts
+++ src/service/objects.ts
@@ -36,25 +36,3 @@ export const createObject = async ({
 		throw new Error(errorMessage)
 	}
 }
-
-export const getBucketContent = async ({ bucket }: { bucket: string }) => {
-	const url = `${BASE_API}/bucket/get`
-
-	const payload = JSON.stringify({ did: bucket })
-
-	const options = {
-		method: "POST",
-		headers: { "content-type": "application/json" },
-		body: payload,
-	}
-
-	try {
-		const response: Response = await fetch(url, options)
-		if (!response.ok) throw new Error(response.statusText)
-		const data = await response.json()
-		return data.bucket
-	} catch (error) {
-		const errorMessage = formatApiError({ error, url, options })
-		throw new Error(errorMessage)
-	}
-}
