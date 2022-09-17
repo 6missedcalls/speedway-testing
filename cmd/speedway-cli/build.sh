c78be44f4b83f5e93785e81dc0d6efa0df35bc20
brokecollegedevwithaclothingobsession <supremehitsheads@gmail.com>
2022-09-17 14:41:44 -0700

    updated build.sh

--- cmd/speedway-cli/build.sh
+++ cmd/speedway-cli/build.sh
@@ -5,7 +5,7 @@ echo "Speedway CLI 🏎  Building..."
 go build -o bin/speedway
 echo "Done!"
 
-echo "Speedway CLI 🏎  Installing..."
-mv bin/speedway ~/go/bin/speedway
+echo "Speedway CLI 🏎  Moving Build..."
+sudo mv bin/speedway /usr/local/bin/
 rm -rf bin
 echo "Done!"
