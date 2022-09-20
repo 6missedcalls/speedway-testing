963cdb98751b513172190aab15d7a15dc17b2e80
mcjcloud <brayden14cloud@gmail.com>
2022-09-20 17:29:52 -0500

    added schema and client code

--- internal/daemon/schema.go
+++ internal/daemon/schema.go
@@ -2,6 +2,7 @@ package daemon
 
 import (
 	"context"
+
 	rtmv1 "github.com/sonr-io/sonr/third_party/types/motor/api/v1"
 )
 
@@ -12,5 +13,17 @@ func (d *Daemon) CreateSchema(req rtmv1.CreateSchemaRequest, res *rtmv1.CreateSc
 
 func (d *Daemon) GetSchema(req rtmv1.QuerySchemaRequest, res *rtmv1.QueryWhatIsResponse) (err error) {
 	*res, err = d.instance.GetSchema(context.Background(), req.Creator, req.Did)
+	return
+}
+
+func (d *Daemon) CreateSchemaDocument(req rtmv1.UploadDocumentRequest, res *rtmv1.UploadDocumentResponse) (err error) {
+	r, e := d.instance.Instance.UploadDocument(req)
+	*res = *r
+	err = e
+	return
+}
+
+func (d *Daemon) GetSchemaDocument(req rtmv1.GetDocumentRequest, res *rtmv1.GetDocumentResponse) (err error) {
+	*res, err = d.instance.GetSchemaDocument(req)
 	return
 }
