d523f162a435b0766758bc9999a0ab3a155a41a6
6missedcalls <brokecollegedev@gmail.com>
2022-09-22 13:51:36 -0700

    hopes and dreams

--- server/routes/server.go
+++ server/routes/server.go
@@ -93,6 +93,13 @@ func (ns *NebulaServer) ConfigureRoutes() error {
 	ns.Router.POST("/api/v1/account/login", ns.LoginAccount)
 	ns.Router.GET("api/v1/account/info", ns.GetAccount)
 
+	// * Alias Routes
+	ns.Router.POST("/api/v1/alias/buy", ns.BuyAlias)
+	ns.Router.POST("/api/v1/alias/sell", ns.SellAlias)
+	ns.Router.POST("/api/v1/alias/transfer", ns.TransferAlias)
+
+	ns.Router.GET("/api/v1/alias/get/:alias", ns.GetAlias)
+
 	// * Schema Routes
 	ns.Router.POST("/api/v1/schema/create", ns.CreateSchema)
 	ns.Router.POST("/api/v1/schema/get", ns.QuerySchema)
