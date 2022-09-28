66d16d8c622e1f11836a598f1974f84b4c5b015f
Stefano Damiano <stefano@ae.studio>
2022-09-28 17:58:50 -0300

    feat: adapting build object to accept byte type, decoding base64 string and converting to []byte

--- server/routes/build-object.go
+++ server/routes/build-object.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"math"
 	"net/http"
+	"encoding/base64"
 
 	"github.com/gin-gonic/gin"
 	rtmv1 "github.com/sonr-io/sonr/third_party/types/motor/api/v1"
@@ -102,6 +103,17 @@ func (ns *NebulaServer) BuildObject(c *gin.Context) {
 				objBuilder.Set(k, value)
 			} else {
 				objBuilder.Set(k, int64(value))
+			}
+			continue
+		case map[string]interface{}:
+			value := v.(map[string]interface{})
+			if base64String, ok :=  value["bytes"]; ok {
+				rawDecodedString, err := base64.StdEncoding.DecodeString(base64String.(string))
+				if err != nil {
+					panic(err)
+				}
+				bytes := []byte(rawDecodedString)
+				objBuilder.Set(k, bytes)
 			}
 			continue
 		default:
