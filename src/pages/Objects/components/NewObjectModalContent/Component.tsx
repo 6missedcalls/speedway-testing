45fc89aa53c327c48a2914c8f936b2af734020ff
Stefano Damiano <stefano@ae.studio>
2022-09-29 19:01:24 -0300

    fix: types on file input

--- src/pages/Objects/components/NewObjectModalContent/Component.tsx
+++ src/pages/Objects/components/NewObjectModalContent/Component.tsx
@@ -150,7 +150,7 @@ function NewObjectModalContentComponent({
 
 type SchemaFieldInputProps = {
 	field: SchemaField
-	onChange: (value: string|Record<string,any>) => void
+	onChange: (value: string | Record<string, any>) => void
 	setError: Dispatch<SetStateAction<string>>
 }
 
