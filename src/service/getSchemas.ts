eddffc1dec6e88938542a846973bec9c668c4ed7
Thiago Fassina <thiagoaaa@gmail.com>
2022-10-01 16:30:31 -0300

    refactor getSchemaMetadata into getSchemas

--- src/service/getSchemas.ts
+++ src/service/getSchemas.ts
@@ -1,45 +1,31 @@
 import { BASE_API } from "../utils/constants"
-import { addressToDid } from "../utils/did"
 import { formatApiError } from "../utils/errors"
 import { SchemaMeta } from "../utils/types"
 
-const mapFieldType = (type: string) => {
-	return {
-		LIST: 0,
-		BOOL: 1,
-		INT: 2,
-		FLOAT: 3,
-		STRING: 4,
-		BYTES: 5,
-		LINK: 6,
-	}[type]
-}
-
-const getSchemaMetadata = async (address: string): Promise<SchemaMeta[]> => {
-	const url = `${BASE_API}/proxy/schemas?pagination.limit=50000`
+const getSchemas = async (address: string): Promise<SchemaMeta[]> => {
+	const url = `${BASE_API}/api/v1/schema/get-from-creator`
 	const options = {
-		method: "GET",
+		method: "POST",
 		headers: { "content-type": "application/json" },
+		body: JSON.stringify({ creator: address }),
 	}
 
 	try {
 		const response = await fetch(url, options)
 		if (!response.ok) throw new Error(response.statusText)
 		const data = await response.json()
-		return data.what_is
-			.filter((schema: any) => schema.creator === addressToDid(address))
-			.map((schema: any) => ({
-				did: schema.schema.did,
-				label: schema.schema.label,
-				fields: schema.schema.fields.map((field: any) => ({
-					name: field.name,
-					type: mapFieldType(field.field),
-				})),
-			}))
+		return data.what_is.map((schema: any) => ({
+			did: schema.schema.did,
+			label: schema.schema.label,
+			fields: schema.schema.fields.map((field: any) => ({
+				name: field.name,
+				type: field.field,
+			})),
+		}))
 	} catch (error) {
 		const errorMessage = formatApiError({ error, url, options })
 		throw new Error(errorMessage)
 	}
 }
 
-export default getSchemaMetadata
+export default getSchemas
