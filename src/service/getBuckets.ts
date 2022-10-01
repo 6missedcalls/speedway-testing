79902a98efe2a802070ebebbe5e4f68efc56a4a7
Thiago Fassina <thiagoaaa@gmail.com>
2022-10-01 16:30:07 -0300

    rollback motorhttp attempt

--- src/service/getBuckets.ts
+++ src/service/getBuckets.ts
@@ -1,20 +1,30 @@
-import MotorHttp from "../MotorHttp"
+import { BASE_API } from "../utils/constants"
+import { formatApiError } from "../utils/errors"
 import { Bucket } from "../utils/types"
 
 const getBuckets = async (address: string): Promise<Bucket[]> => {
-	const response = await MotorHttp.QueryWhereIsByCreator({
-		creator: address,
-	})
+	const url = `${BASE_API}/api/v1/bucket/get-from-creator`
+	const options = {
+		method: "POST",
+		headers: { "content-type": "application/json" },
+		body: JSON.stringify({ creator: address }),
+	}
 
-	if (!response.whereIs) return []
-
-	return response.whereIs.map((bucket) => ({
-		did: bucket.did,
-		label: bucket.label,
-		creator: bucket.creator,
-		timestamp: bucket.timestamp,
-		content: bucket.content ? bucket.content.filter((c) => c.uri) : [],
-	}))
+	try {
+		const response = await fetch(url, options)
+		if (!response.ok) throw new Error(response.statusText)
+		const data = await response.json()
+		return data.where_is.map((bucket: any) => ({
+			did: bucket.did,
+			label: bucket.label,
+			creator: bucket.creator,
+			timestamp: bucket.timestamp,
+			content: bucket.content ? bucket.content.filter((c: any) => c.uri) : [],
+		}))
+	} catch (error) {
+		const errorMessage = formatApiError({ error, url, options })
+		throw new Error(errorMessage)
+	}
 }
 
 export default getBuckets
